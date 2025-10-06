43093049e8ae286f1275e54ecc322a921cc3ef03
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:37:14 -0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include("header.php");
+include("home.php");
+include()
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+           <!-- Botão que redireciona para cadastro.php -->
+<form action="cadastro.php" method="get">
+    <button type="submit">Ir para Cadastro</button>
+</form>
+
+                <a href="#">Esqueceu a senha?</a>
+                <span>Não tem uma conta? <a href="#">Cadastre-se</a></span>
+            </div>
+        </form>
+    </div>
+  </div>
+           <!-- Botão que redireciona para cadastro.php -->
+     
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
+
+</body>
+</html>
