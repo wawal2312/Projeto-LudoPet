c863f7019843d0d053647c551bf5d64c59d77200
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:39:59 -0300

    Create nossotrabalho.php

---
+++ nossotrabalho.php
@@ -0,0 +1,64 @@
+<?php
+echo "Nosso trabalho";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+           
+    Nosso Trabalho
+</h1>
+<h4>
+    Nosso trabalho é voltado à criação de uma plataforma informativa, acessível e acolhedora para pessoas que convivem com o
+Transtorno do Espectro Autista (TEA). Reunimos, organizamos e disponibilizamos conteúdos e contatos úteis para orientar pais,
+mães e cuidadores em suas jornadas.
+</h4>
+<h2>
+    As principais frentes do nosso trabalho são:
+</h2>
+<h4>
+ Mapeamento de recursos locais
+<br>
+🧭 Fazemos a curadoria de clínicas, terapeutas, centros especializados, escolas inclusivas e profissionais de saúde em São Luís –
+MA, facilitando o acesso a serviços confiáveis.
+<br>
+<br>
+📚 Produção de conteúdo educativo
+<br>
+ Desenvolvemos textos informativos sobre o autismo, seus sintomas, os níveis do espectro, formas de diagnóstico, terapias
+recomendadas e direitos da pessoa com TEA.
+<br>
+<br>
+🌐 Desenvolvimento de plataforma digital
+<br>
+ Como estudantes de Engenharia da Computação, aplicamos nossos conhecimentos em tecnologia para criar um site funcional,
+de fácil navegação e pensado para o público leigo, especialmente pais e familiares em busca de orientação.
+<br>
+<br>
+🤝 Promoção da inclusão e da empatia
+<br>
+ Nosso projeto também busca conscientizar a sociedade sobre o autismo, quebrar estigmas e valorizar a neurodiversidade.
+Acreditamos que a informação é uma ponte para o respeito e a inclusão.
+<br>
+Este projeto é fruto de muito estudo, empatia e dedicação. Continuamos aprimorando nossa plataforma com base em
+feedbacks e nas reais necessidades das famílias.
+<br>
+Se você tem uma sugestão, dúvida ou deseja colaborar, entre em contato conosco. Juntos, podemos fazer a diferença.
+</h4>
+           
+
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
