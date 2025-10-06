3d05ca4c26f1302476be62cd07661cf3da99251a
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:39:07 -0300

    Create ludopet.php

---
+++ ludopet.php
@@ -0,0 +1,54 @@
+<?php
+echo "O que é o autismo?";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+           <!-- Botão que redireciona para cadastro.php -->
+<h1>
+    O que é o Transtorno do Espectro Autista (TEA)?
+</h1>
+<h4>
+    O Transtorno do Espectro Autista (TEA) é uma condição neurológica e do desenvolvimento que acompanha a pessoa por toda a vida. Ele
+afeta principalmente a comunicação, a interação social, o comportamento e a forma como o indivíduo percebe o ambiente ao seu redor.
+O termo "espectro" é utilizado porque existem diferentes formas e graus de manifestação do autismo, que variam de pessoa para
+pessoa.
+</h4>
+<h2>
+    Pessoas com TEA podem apresentar:
+</h2>
+<h4>
+-Dificuldade em iniciar ou manter interações sociais.
+
+-Atrasos na fala ou ausência de linguagem verbal.
+
+-Interesses restritos e comportamentos repetitivos.
+
+-Resistência a mudanças na rotina.
+
+-Reações intensas a estímulos sensoriais, como sons, luzes ou texturas.
+
+
+Apesar desses desafios, muitas pessoas autistas têm habilidades únicas e podem se destacar em áreas como lógica, arte, memorização,
+tecnologia e outras, especialmente quando recebem suporte adequado.
+É importante lembrar: o autismo não é uma doença. Ele não tem cura, mas tem tratamento, acompanhamento e acolhimento que fazem
+toda a diferença no desenvolvimento e qualidade de vida da pessoa autista.
+</h4>
+           <!-- Botão que redireciona para cadastro.php -->
+
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
