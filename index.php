26c6cfa86f469cb54724a4adaedbdda60e6bf7e8
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:37:49 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include("header.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <link rel="stylesheet" href="css2.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+           <!-- Botão que redireciona para cadastro.php -->
+            <div class="container-A"> 
+<form action="cadastro.php" method="get" class="item-A">
+    <button type="submit">Ir para Cadastro</button>
+</form>
+</div>
+                <a href="#" class="container-A">Esqueceu a senha?</a>
+                <span class="container-A">Não tem uma conta? <a href="#">Cadastre-se</a></span>
+            </div>
+        </form>
+    </div>
+  </div>
+           <!-- Botão que redireciona para cadastro.php -->
+
+
+            </div>
+            <div class="container-A"> 
+            <form action="oqeautismo.php" method="get" class="item-A">
+   <button type="submit">O que é o autismo?</button>
+   </form>
+            <form action="quemsomos.php" method="get" class="item-A">
+   <button type="submit">Quem somos?</button>
+   </form>
+            <form action="nossotrabalho.php" method="get" class="item-A">
+   <button type="submit">Nosso trabalho</button>
+   </form>
+   </form>
+            <form action="objetivo.php" method="get" class="item-A">
+   <button type="submit">Nosso Objetivo</button>
+   </form>
+   </form>
+   </div>
+    </div>
+ </body>
+</html>
