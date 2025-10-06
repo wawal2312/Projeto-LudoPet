0ad30e3f09dbacdc67d902b1d3d6d265a86e6709
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:36:20 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="input-group">
+    <div class="input-group-text" id="btnGroupAddon">Pesquisar</div>
+    <input type="text" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon">
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <form action="Home.php" method="get">
+   <button type="button" class="btn btn-link">Home</button>
+   </form>
+   </div>
+</body>
+</html>
