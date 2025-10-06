d90368e74b977175ee46311abe45c73e90a5a745
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:40:36 -0300

    Create objetivo.php

---
+++ objetivo.php
@@ -0,0 +1,45 @@
+<?php
+echo "Nosso objetivo";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+<h1>
+    Nosso Objetivo
+</h1>
+<h4>
+    Somos estudantes do curso de Engenharia da Computação, movidos pelo desejo de aplicar a tecnologia para
+transformar vidas de maneira positiva. Nosso objetivo com este projeto é ajudar pais, mães e familiares de pessoas
+autistas a acessarem informações confiáveis, atualizadas e organizadas sobre clínicas, tratamentos, terapias,
+profissionais e outros recursos disponíveis em São Luís – MA.
+<br>
+<br>
+Sabemos que o diagnóstico do Transtorno do Espectro Autista (TEA) pode trazer muitas dúvidas, inseguranças e
+desafios para as famílias. Acreditamos que a informação de qualidade é uma das principais formas de apoio, por isso
+nos dedicamos a criar uma plataforma digital que facilite o acesso a esses dados de forma clara, simples e inclusiva.
+<br>
+Queremos que este site seja um ponto de partida, um canal de apoio e orientação. Um lugar onde mães, pais e
+responsáveis encontrem suporte, acolhimento e caminhos possíveis para cuidar, entender e acompanhar seus filhos
+autistas com mais confiança e tranquilidade.
+<br>   
+Nosso compromisso é com o respeito à neurodiversidade, à inclusão e à construção de uma sociedade mais empática e
+informada. 
+</h4>
+
+
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
