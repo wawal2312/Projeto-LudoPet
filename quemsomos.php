a20156fb69cb6df4407853cec095712d0317f4b3
Vitoria1705 <escorcio545@gmail.com>
2025-10-06 20:42:59 -0300

    Create quemsomos.php

---
+++ quemsomos.php
@@ -0,0 +1,49 @@
+<?php
+echo "Quem somos";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Espectro</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+ <body>
+ </div>
+<h1>
+    Quem Somos
+</h1>
+<h4>
+    Somos um grupo de estudantes do curso de Engenharia da Computação apaixonados por tecnologia e movidos por
+causas humanas. Este projeto nasceu de uma inquietação pessoal e social: percebemos que muitas famílias com filhos
+autistas enfrentam barreiras para encontrar informações confiáveis e acessíveis sobre diagnóstico, tratamento e apoio
+especializado, especialmente em nossa cidade, São Luís – MA.
+<br>
+Com base nessa realidade, decidimos unir nossos conhecimentos técnicos com nosso desejo de contribuir com algo
+significativo. Assim, desenvolvemos este site com o objetivo de organizar e compartilhar informações úteis para pais e
+mães de pessoas autistas.
+<br>
+<br>
+Nossa equipe atua na curadoria de dados, desenvolvimento da plataforma e produção de conteúdos relevantes sobre
+o autismo. Trabalhamos com seriedade, responsabilidade e sensibilidade, sempre em busca de aprimoramento
+constante.
+<br>
+Acreditamos que a tecnologia deve servir à sociedade. Por isso, nosso foco não é apenas informar, mas acolher e
+empoderar famílias, mostrando que não estão sozinhas e que existem caminhos possíveis para o cuidado e o
+desenvolvimento dos seus filhos.
+<br>
+Estamos abertos a parcerias, sugestões e colaborações que possam enriquecer este projeto. Juntos, podemos construir
+uma rede mais forte de apoio e conhecimento.
+</h4>
+
+
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
