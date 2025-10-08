ac1e5729f9a6a55efaaf31738b63c3b1e64a7bc4
Vitoria1705 <escorcio545@gmail.com>
2025-10-08 18:29:34 -0300

    Update header.php

--- header.php
+++ header.php
@@ -8,31 +8,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="projeto.css">
   </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">🐾 LudoPet</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link text-nowrap" href="index.php">Início</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="adocao.php">Adoção</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="doacao.php">Doação</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="quemsomos.php">Quem Somos</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="ludopet.php">Sobre o LudoPet</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="nossotrabalho.php">Nosso Trabalho</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="objetivo.php">Nosso Objetivo</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="perdidos.php">Animais Perdidos</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="parceiros.php">Parceiros</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="forum.php">Fórum</a></li>
-        <li class="nav-item"><a class="nav-link text-nowrap" href="contato.php">Contato</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <a class="navbar-brand fw-bold" href="index.php">🐾 LudoPet</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link text-nowrap" href="index.php">Início</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="adocao.php">Adoção</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="doacao.php">Doação</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="quemsomos.php">Quem Somos</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="ludopet.php">Sobre o LudoPet</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="nossotrabalho.php">Nosso Trabalho</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="objetivo.php">Nosso Objetivo</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="perdidos.php">Animais Perdidos</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="parceiros.php">Parceiros</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="forum.php">Fórum</a></li>
+            <li class="nav-item"><a class="nav-link text-nowrap" href="contato.php">Contato</a></li>
+          </ul>
         </div>
       </div>
     </nav>
+    <!-- Fim da Navbar -->
